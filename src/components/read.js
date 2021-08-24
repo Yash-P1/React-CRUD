2cb386b74f6bfb2487a28d8ed4235c39eb520236
Yash Patel <yspatel97@gmail.com>
2021-08-24 01:45:20 -0400

    First commit

---
+++ src/components/read.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { Table, Button } from "semantic-ui-react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function Read() {
+  const [APIData, setAPIData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://61232d99d446280017054a3b.mockapi.io/fakeUsers`)
+      .then((response) => {
+        setAPIData(response.data);
+      });
+  }, []);
+
+  const setData = (data) => {
+    let { id, firstName, lastName, checkbox } = data;
+    localStorage.setItem("ID", id);
+    localStorage.setItem("First Name", firstName);
+    localStorage.setItem("Last Name", lastName);
+    localStorage.setItem("Checkbox Value", checkbox);
+  };
+
+  const onDelete = (id) => {
+    axios
+      .delete(`https://61232d99d446280017054a3b.mockapi.io/fakeUsers/${id}`)
+      .then(() => {
+        getData();
+      });
+  };
+
+  const getData = () => {
+    axios
+      .get(`https://61232d99d446280017054a3b.mockapi.io/fakeUsers/`)
+      .then((getData) => {
+        setAPIData(getData.data);
+      });
+  };
+
+  return (
+    <div>
+      <Table singleLine>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>First Name</Table.HeaderCell>
+            <Table.HeaderCell>Last Name</Table.HeaderCell>
+            <Table.HeaderCell>Checked</Table.HeaderCell>
+            <Table.HeaderCell>Updation</Table.HeaderCell>
+            <Table.HeaderCell>Deletion</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {APIData.map((data) => {
+            return (
+              <Table.Row>
+                <Table.Cell>{data.firstName}</Table.Cell>
+                <Table.Cell>{data.lastName}</Table.Cell>
+                <Table.Cell>
+                  {data.checkbox ? "Checked" : "Unchecked"}
+                </Table.Cell>
+                <Link to="/update">
+                  <Table.Cell>
+                    <Button onClick={() => setData(data)}>Update</Button>
+                  </Table.Cell>
+                </Link>
+                <Table.Cell>
+                  <Button onClick={() => onDelete(data.id)}>Delete</Button>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+}
